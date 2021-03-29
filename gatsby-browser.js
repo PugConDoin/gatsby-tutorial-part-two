304b2bde77e367c9c550a2f2325797fbded24d13
Mark Freeman <dorsettri@gmail.com>
2021-03-29 06:55:43 +0100

    add gatsby-browser, components and css-modules

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+// lets you respond to actions within the browser, and wrap your site in additional components. The Gatsby Browser API gives you many options for interacting with the client side.
+// To use Browser APIs, create a file in the root of your site at gatsby-browser.js. Export each API you want to use from this file.
+// If running files in a Node.js only environment use...
+// require('./src/styles/global.css')
+
+import "./src/styles/global.css"
